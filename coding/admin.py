eab5bb6f244e0bafee85993a614d873bba9084c8
admin <you@example.com>
2023-04-29 23:11:52 +0200

    Modificato modeladmin di coding, modifica stile snippet
    Aggiornato admin.py per visualizzare linguaggi e argomenti, introdotti filtri, ordinamento e ricerca; nel template coding_list.html cambiato background e colore del campo snippet

--- coding/admin.py
+++ coding/admin.py
@@ -6,3 +6,9 @@ from .models import Coding
 class SetAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug': ('linguaggio',), 'slug_argomento': ('argomento',)}
     # prepopulated_fields={'slug_argomento': ('argomento',)}
+
+    list_display = ('linguaggio', 'argomento',)
+    ordering = ('linguaggio',)
+
+    list_filter = ('linguaggio', 'argomento',)
+    search_fields = ('argomento',)
